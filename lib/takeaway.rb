339d09b3d2e2600b658bf7e60c914c72e3d02d91
Canvasbag989 <Canvasbag989@icloud.com>
2019-09-02 08:42:28 +0100

    started creating second test

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,7 +2,7 @@ class Takeaway
 
   attr_reader :menu
 
-  CURRENT_MENU = { "chips" => "£2", "burger" => "£4", "half chicken" => "£3.50" }
+  CURRENT_MENU = { "chips" => "£2", "burger" => "£4", "half chicken" => "£3" }
 
   def initialize
     @menu = CURRENT_MENU
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,6 +2,9 @@ require 'takeaway'
 
 describe Takeaway do
   it 'has a menu' do
-    expect(subject.menu).to eq({ "chips" => "£2", "burger" => "£4", "half chicken" => "£3.50" })
+    expect(subject.menu).to eq({ "chips" => "£2", "burger" => "£4", "half chicken" => "£3" })
+  end
+  it 'creates an order' do
+
   end
 end
